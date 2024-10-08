d1a7581af0700974b5372ffd76d5733aaf9ab17d
Monirul Jim <monirulislamjim6640@gmail.com>
2024-10-08 20:48:15 +0600

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      eqeqeq: "off",
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      "no-undef": "error",
+    },
+    "globals": {
+      "process": "readonly"
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
