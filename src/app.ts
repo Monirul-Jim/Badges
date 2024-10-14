74d1f3c468736b8770507683bb0e6052338aea2a
Monirul Jim <monirulislamjim6640@gmail.com>
2024-10-14 14:37:11 +0600

    make index.ts route for better api handling

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,13 @@ import { StudentRoutes } from "./app/modules/student/student.route";
 import { UserRoutes } from "./app/modules/user/user.routes";
 import globalErrorHandler from "./app/middleware/globalErrorHandler";
 import notFound from "./app/middleware/notFound";
+import router from "./app/routes";
 const app: Application = express();
 const port = 3000;
 // parsers
 app.use(express.json());
 app.use(cors());
-app.use("/api/v1/students", StudentRoutes);
-app.use("/api/v1/users", UserRoutes);
+app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
