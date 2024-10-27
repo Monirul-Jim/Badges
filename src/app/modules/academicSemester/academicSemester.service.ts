a8e4e5ff4ea3439c52d3295255ae8f49bca02493
Monirul Jim <monirulislamjim6640@gmail.com>
2024-10-27 23:30:47 +0600

    handle right semester code as semseter name wise

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,7 +1,13 @@
-import { TAcademicSemesterCode } from "./academicSemester.interface";
+import { academicSemesterNameCodeMapper } from "./academicSemester.constant";
+import { TAcademicSemester } from "./academicSemester.interface";
 import { AcademicSemester } from "./academicSemester.models";
 
-const createAcademicSemesterIntoDB = async (payload: TAcademicSemesterCode) => {
+const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
+  // semester name and semester code is right
+
+  if (academicSemesterNameCodeMapper[payload.name] !== payload.code) {
+    throw new Error("Invalid semester code !");
+  }
   const result = await AcademicSemester.create(payload);
   return result;
 };
