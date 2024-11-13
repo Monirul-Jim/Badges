438410b607fbafaccf13bfbbf83616824cf5eee9
Monirul Jim <monirulislamjim6640@gmail.com>
2024-11-13 23:02:23 +0600

    add course interface and model

---
+++ src/app/modules/course/course.model.ts
@@ -0,0 +1,35 @@
+import { model, Schema, Types } from "mongoose";
+import { TCourse, TPreRequisiteCourses } from "./course.interface";
+
+const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourses>({
+  course: {
+    type: Schema.ObjectId,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+const courseSchema = new Schema<TCourse>({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  prefix: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  code: {
+    type: Number,
+    required: true,
+  },
+  credits: {
+    type: Number,
+    required: true,
+  },
+  preRequisiteCourses: [preRequisiteCoursesSchema],
+});
+export const Course = model<TCourse>("Course", courseSchema);
