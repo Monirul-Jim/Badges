3e5c27f36f9d69f56209bdcfbe7dbf4c9a2fc20d
Monirul Jim <monirulislamjim6640@gmail.com>
2024-11-26 23:02:11 +0600

    when update course update delete use transaction and rollback

--- src/app/modules/course/course.model.ts
+++ src/app/modules/course/course.model.ts
@@ -1,42 +1,53 @@
 import { model, Schema, Types } from "mongoose";
 import { TCourse, TPreRequisiteCourses } from "./course.interface";
 
-const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourses>({
-  course: {
-    type: Schema.Types.ObjectId,
-    ref: "Course",
+const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourses>(
+  {
+    course: {
+      type: Schema.Types.ObjectId,
+      ref: "Course",
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  isDeleted: {
-    type: Boolean,
-    default: false,
+  {
+    _id: false,
+    timestamps: true,
+  }
+);
+const courseSchema = new Schema<TCourse>(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    prefix: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    code: {
+      type: Number,
+      trim: true,
+      required: true,
+    },
+    credits: {
+      type: Number,
+      trim: true,
+      required: true,
+    },
+    preRequisiteCourses: [preRequisiteCoursesSchema],
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-});
-const courseSchema = new Schema<TCourse>({
-  title: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  prefix: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  code: {
-    type: Number,
-    trim: true,
-    required: true,
-  },
-  credits: {
-    type: Number,
-    trim: true,
-    required: true,
-  },
-  preRequisiteCourses: [preRequisiteCoursesSchema],
-  isDeleted: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 export const Course = model<TCourse>("Course", courseSchema);
--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -1,7 +1,9 @@
+import mongoose from "mongoose";
 import QueryBuilder from "../../../builder/QueryBuilder";
 import { CourseSearchableFields } from "./course.constant";
 import { TCourse } from "./course.interface";
 import { Course } from "./course.model";
+import AppError from "../../errors/AppError";
 
 const createCourseIntoDB = async (payload: TCourse) => {
   const result = await Course.create(payload);
@@ -40,25 +42,72 @@ const deleteCourseFromDB = async (id: string) => {
 };
 const updateCourseIntoDB = async (id: string, payload: Partial<TCourse>) => {
   const { preRequisiteCourses, ...courseRemainingData } = payload;
-  // basic course info update
-  const updatedBasicCourseInfo = await Course.findByIdAndUpdate(
-    id,
-    courseRemainingData,
-    {
-      new: true,
-      runValidators: true,
-    }
-  );
-  if (preRequisiteCourses && preRequisiteCourses.length > 0) {
-    const deletedPreRequisites = preRequisiteCourses
-      .filter((el) => el.course && el.isDeleted)
-      .map((el) => el.course);
-    const deletedPreRequisiteCourses = await Course.findByIdAndUpdate(id, {
-      $pull: { preRequisiteCourses: { course: { $in: deletedPreRequisites } } },
-    });
-  }
+  const session = await mongoose.startSession();
 
-  return updatedBasicCourseInfo;
+  try {
+    await session.startTransaction();
+    // basic course info update
+    const updatedBasicCourseInfo = await Course.findByIdAndUpdate(
+      id,
+      courseRemainingData,
+      {
+        new: true,
+        runValidators: true,
+        session,
+      }
+    );
+    if (!updatedBasicCourseInfo) {
+      throw new AppError(400, "Failed To Update Course");
+    }
+    if (preRequisiteCourses && preRequisiteCourses.length > 0) {
+      const deletedPreRequisites = preRequisiteCourses
+        .filter((el) => el.course && el.isDeleted)
+        .map((el) => el.course);
+      const deletedPreRequisiteCourses = await Course.findByIdAndUpdate(
+        id,
+        {
+          $pull: {
+            preRequisiteCourses: { course: { $in: deletedPreRequisites } },
+          },
+        },
+        {
+          new: true,
+          runValidators: true,
+          session,
+        }
+      );
+      if (!deletedPreRequisiteCourses) {
+        throw new AppError(400, "Failed To Update Course");
+      }
+      const newPreRequisite = preRequisiteCourses?.filter(
+        (el) => el.course && !el.isDeleted
+      );
+      const newPreRequisiteCourses = await Course.findByIdAndUpdate(
+        id,
+        {
+          $addToSet: { preRequisiteCourses: { $each: newPreRequisite } },
+        },
+        {
+          new: true,
+          runValidators: true,
+          session,
+        }
+      );
+      if (!newPreRequisiteCourses) {
+        throw new AppError(400, "Failed To Update Course");
+      }
+    }
+    await session.commitTransaction();
+    await session.endSession();
+    const result = await Course.findById(id).populate(
+      "preRequisiteCourses.course"
+    );
+    return result;
+  } catch (err) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new AppError(400, "Failed To Update Course");
+  }
 };
 export const CourseServices = {
   createCourseIntoDB,
